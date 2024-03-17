bc9f924c4ccd1a3de56ea658ff7ca99a284464cf
martin de la gorce <madelago@microsoft.com>
2024-03-17 15:44:50 +0000

    get the vrsion from the init file

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 """Setup script."""
 
+import os
+import re
+
 from setuptools import find_packages, setup
 
 libname = "openexr_numpy"
 
+# get the version from the __init__ file
+# using a regular expression
+with open(os.path.join(os.path.dirname(__file__), libname, "__init__.py")) as fp:
+    for line in fp:
+        m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
+        if m:
+            version = m.group(2)
+            break
+    else:
+        raise RuntimeError("Unable to find own __version__ string")
+
 setup(
     name=libname,
-    version="0.0.1",
+    version=version,
     author="Martin de La Gorce",
     author_email="martin.delagorce@gmail.com",
     description="Module read and write Open EXR image files using numpy arrays.",

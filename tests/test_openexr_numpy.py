1c48b4d428b3e653df480771f9a666701c66ebd9
martin de la gorce <madelago@microsoft.com>
2024-03-17 11:01:11 +0000

    test if ci fails

--- tests/test_openexr_numpy.py
+++ tests/test_openexr_numpy.py
@@ -125,7 +125,7 @@ def test_exr() -> None:
     # step and modifies the system path)
     # imageio opencv's plugin loads the image as unit8
     rgba_image = np.random.rand(12, 30, 3).astype(np.float32)
-    file_path = "test.exr"
+    file_path ="test.exr"
     imageio.imwrite(file_path, rgba_image)
     rgba_image_a = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)[:, :, ::-1]
     rgba_image_b = imread(file_path)

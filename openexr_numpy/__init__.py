71532407a3467b266bafd5e8d403ca342b619f58
martin de la gorce <madelago@microsoft.com>
2024-03-17 02:41:11 +0000

    Update __init__.py

---
+++ openexr_numpy/__init__.py
@@ -0,0 +1,13 @@
+"""openexr_numpy.
+
+Martin de La Gorce. 2024.
+"""
+__version__ = "0.0.1"
+
+__all__ = [
+    "imread",
+    "imwrite",
+    "set_default_channel_names"
+]
+
+from .openexr_numpy import imread, imwrite, set_default_channel_names
